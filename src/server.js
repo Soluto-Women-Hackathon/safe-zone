e6acf9d4e2d0d955012630c610706f48c4cf4581
Lior Shub <liorbentov@gmail.com>
2018-03-08 19:13:01 +0200

    Add basic telegram logic

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const Telegraf = require('telegraf');
+const Extra = require('telegraf/extra');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const COMPLAIN_COMMAND = 'complain';
+
+bot.start(ctx => {
+    return ctx.reply('Safe Zone', Extra.markup((markup) => {
+        return markup.resize()
+            .keyboard([
+                markup.callbackButton(COMPLAIN_COMMAND, COMPLAIN_COMMAND),
+                markup.locationRequestButton('🔍 Am I safe?')
+            ])
+    })
+)});
+
+bot.on('location', ctx => ctx.reply('You are in a safe zone'));
+bot.hears(COMPLAIN_COMMAND, (ctx) => ctx.reply('Buy-buy!'));
+
+bot.startPolling();
+
