0046cd55a49ffcf2b8453cf0d40e791fe5a34710
Lior Shub <liorbentov@gmail.com>
2018-03-08 19:29:40 +0200

    Add database

---
+++ src/database.js
@@ -0,0 +1,16 @@
+const MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+const url = process.env.DATABASE_URL;
+
+// Database Name
+const dbName = process.env.PROJECT_NAME;
+
+// Use connect method to connect to the server
+MongoClient.connect(url, err, client => {
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    client.close();
+});
